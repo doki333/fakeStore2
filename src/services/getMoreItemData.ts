1d5ac797ec5e8bfd0ec6c72375ea9113989fea6d
doki333 <duckky333@gmail.com>
2022-06-04 14:23:08 +0900

    fix: call api func

--- src/services/getMoreItemData.ts
+++ src/services/getMoreItemData.ts
@@ -6,12 +6,12 @@ interface IProps {
   code: number | null
 }
 
-const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy'
+const PROXY = window.location.hostname === 'localhost' ? '/api/v1' : '/proxy'
 
 const getMoreItemData = async ({ pageParam, code }: IProps) => {
   const prevAddr = code === null ? '' : `categories/${code}/`
   const response = await axios
-    .get(`${PROXY}/api/v1/${prevAddr}products?offset=${pageParam}&limit=20`, {
+    .get(`${PROXY}${prevAddr}products?offset=${pageParam}&limit=20`, {
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
