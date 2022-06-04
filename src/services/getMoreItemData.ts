17a3b321859c601922a59a4fef1092bab9b53bad
doki333 <duckky333@gmail.com>
2022-06-04 12:33:08 +0900

    fix: netlify.toml

--- src/services/getMoreItemData.ts
+++ src/services/getMoreItemData.ts
@@ -6,13 +6,15 @@ interface IProps {
   code: number | null
 }
 
-const PROXY = window.location.hostname === 'localhost' ? '/api/v1' : '/proxy'
+const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy'
 
 const getMoreItemData = async ({ pageParam, code }: IProps) => {
   const prevAddr = code === null ? '' : `/categories/${code}`
   const response = await axios
     .get(`${PROXY}${prevAddr}/products?offset=${pageParam}&limit=20`)
     .then((res) => {
+      // eslint-disable-next-line no-console
+      console.log(res)
       return res.data
     })
     .catch(() => {
