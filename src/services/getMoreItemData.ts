7e84c93288ed2554ac30ded33871902034dba310
doki333 <duckky333@gmail.com>
2022-06-04 15:19:54 +0900

    fix: getMoreData func

--- src/services/getMoreItemData.ts
+++ src/services/getMoreItemData.ts
@@ -9,7 +9,7 @@ interface IProps {
 const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy'
 
 const getMoreItemData = async ({ pageParam, code }: IProps) => {
-  const prevAddr = code === null && `/categories/${code}`
+  const prevAddr = code === null ? '' : `/categories/${code}`
   const response = await axios
     .get(`${PROXY}${prevAddr}/products?offset=${pageParam}&limit=20`)
     .then((res) => {
