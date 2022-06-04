a2893de655e274ab6aa5f6a446809e05b89e7b1d
doki333 <duckky333@gmail.com>
2022-06-04 14:31:31 +0900

    fix: getMoreItemData

--- src/services/getMoreItemData.ts
+++ src/services/getMoreItemData.ts
@@ -6,20 +6,13 @@ interface IProps {
   code: number | null
 }
 
-const PROXY = window.location.hostname === 'localhost' ? '/api/v1/' : '/proxy/'
+const PROXY = window.location.hostname === 'localhost' ? '/api/v1' : '/proxy'
 
 const getMoreItemData = async ({ pageParam, code }: IProps) => {
-  const prevAddr = code === null ? '' : `categories/${code}/`
+  const prevAddr = code === null ? '' : `/categories/${code}`
   const response = await axios
-    .get(`${PROXY}${prevAddr}products?offset=${pageParam}&limit=20`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    })
+    .get(`${PROXY}${prevAddr}/products?offset=${pageParam}&limit=20`)
     .then((res) => {
-      // eslint-disable-next-line no-console
-      console.log(res)
       return res.data
     })
     .catch(() => {
