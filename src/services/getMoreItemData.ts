1d9560b4536ba0c59fb3b7505aa4d6e47c73d2a7
doki333 <duckky333@gmail.com>
2022-06-04 15:17:06 +0900

    fix: getMoreData func

--- src/services/getMoreItemData.ts
+++ src/services/getMoreItemData.ts
@@ -9,9 +9,9 @@ interface IProps {
 const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy'
 
 const getMoreItemData = async ({ pageParam, code }: IProps) => {
-  const prevAddr = code === null ? `${PROXY}` : `${PROXY}/categories/${code}`
+  const prevAddr = code === null && `/categories/${code}`
   const response = await axios
-    .get(`${prevAddr}/products?offset=${pageParam}&limit=20`)
+    .get(`${PROXY}${prevAddr}/products?offset=${pageParam}&limit=20`)
     .then((res) => {
       // eslint-disable-next-line no-console
       console.log(res)
